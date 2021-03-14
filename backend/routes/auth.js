ce3d67ef8cbf7b3b34d6df0f9b5e4c997c8738f4
mutebi nuhu <mutebinuhu1@gmail.com>
2021-03-14 05:44:16 +0300

    adding validation messages to the register api

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,11 +1,35 @@
 const express = require('express');
 const Router = express.Router();
+const {check, validationResult} = require('express-validator')
 const User = require('../models/User');
-Router.post('/register', (req, res)=>{
+//for hashing the password
+const bycrpt = require('bcryptjs');
+Router.post('/register',[
+//validation and sanitizing
+check('email').isEmail().isLength({min:5}).withMessage('a minimum of five characters is required').trim().escape(),
+check('password').isLength({min:8}).withMessage('a minimum of eight characters is required').trim().escape()
+
+	],  (req, res)=>{
+	const errors = validationResult(req);
+	if(!errors.isEmpty()){
+		res.status(400).json({
+			success:"False",
+			message:'Registration Failed',
+			errors:errors.array()
+		})
+	}
 	const {email, password} = req.body;
+
 	User.create({
-		email,password`
-	}).then(user=>res.send({data:user}))
+		email,
+		password:bycrpt.hashSync(password, 8)
+	}).then(user=>res.status(200).json(
+		{
+			success:"True",
+			message:'Registration Successful',
+			data:user
+		}
+		))
 	.catch(err=>console.log(err));
 });
 module.exports = Router;
