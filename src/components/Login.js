0bcf24629470414af16756ca36640b80d01ce295
mutebi nuhu <mutebinuhu1@gmail.com>
2021-03-14 21:14:21 +0300

    modifying the login form

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,19 +1,32 @@
 import React from 'react';
-export const Login = () =>{
+import {Button, Form} from 'antd';
+const FormItem = Form.Item;
 
+
+export const Login = () =>{
+	const handleFormSubmit = (values)=>{
+		console.log(values)
+	}
 	return(
-	
 		<>
 			<div class="wrapper fadeInDown">
 			  <div id="formContent">
 			  <div class="fadeIn first">
-      			<img src="https://icon-library.net/images/google-login-icon/google-login-icon-0.jpg" id="icon" alt="User Icon" style={{width:100, height:100}} />
+      			<img src="https://www.pngitem.com/pimgs/m/105-1055419_user-avatar-login-account-profile-people-login-people.png" id="icon" alt="User Icon" style={{width:100, height:100}} />
     			</div>
-    			<form method = 'POST' action='http://localhost:5000/login'>
-			      <input type="text" id="login" class="fadeIn second" name="email" placeholder="Email" />
-			      <input type="password" id="password" class="fadeIn third" name="password" placeholder="password" />
-			      <input type="submit" class="fadeIn fourth" value="Log In" />
-    			</form>
+    			<Form onFinish={(values)=>handleFormSubmit(values)}>
+					<FormItem label="Email" name="email">
+						<input type="text"  required/>
+					</FormItem>
+					<FormItem label="Password" name="password">
+						<input type="password"  required/>
+					</FormItem>
+					<FormItem>
+						<Button type="primary" htmlType="submit" className='btn btn-primary'>
+						  Login
+						</Button>
+					</FormItem>
+				</Form>
     			<div id="formFooter">
       				<a class="underlineHover" href="#">Forgot Password?</a>
     			</div>
