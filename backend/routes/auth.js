3012b7f3474ee40afc23470c30e98283a01eb186
mutebi nuhu <mutebinuhu1@gmail.com>
2021-03-14 04:27:03 +0300

    adding the register api

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -4,7 +4,7 @@ const User = require('../models/User');
 Router.post('/register', (req, res)=>{
 	const {email, password} = req.body;
 	User.create({
-		email,password
+		email,password`
 	}).then(user=>res.send({data:user}))
 	.catch(err=>console.log(err));
 });
